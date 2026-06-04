6de1a7e0913d8fedb6dabfdfd4ba72fbbecd9062
dhikoh <dhiko.h@gmail.com>
2026-06-04 09:40:28 +0700

    chore: ensure storage symlink is generated on container start

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 set -e
 
+# Create storage symlink if it doesn't exist
+php artisan storage:link --force
+
 # Start supervisord to launch Nginx and PHP-FPM
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
