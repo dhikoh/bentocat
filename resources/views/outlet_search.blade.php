faf095ecb8d7ab1e150ed7f1f14e090732400514
dhikoh <dhiko.h@gmail.com>
2026-06-05 00:32:43 +0700

    fix: correct leaflet.js CDN integrity hashes

--- resources/views/outlet_search.blade.php
+++ resources/views/outlet_search.blade.php
@@ -147,7 +147,7 @@
 
 @section('scripts')
 <!-- Leaflet Map JS -->
-<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin=""></script>
+<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
 
 <script>
     // Lead ID passed from controller
